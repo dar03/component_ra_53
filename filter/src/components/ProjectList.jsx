aac41871d65cc3689640020cb1ec0a53a3bfa45c
Alex Dar <dar3105@mail.ru>
2023-06-21 10:47:12 +0800

    Portfolio commit

---
+++ filter/src/components/ProjectList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/ProjectList.css";
+
+function ProjectList({ projects }) {
+    let index = 0;
+  
+    return (
+      projects.map((item) => (
+        <div className='project__img' key={index++}>
+          <img src={item.img} alt='' />
+        </div>
+      ))
+    );
+  }
+  
+  export default ProjectList;
---
+++ filter/src/components/Toolbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/Toolbar.css";
+
+function Toolbar({ filters, selected, onSelectFilter }) {
+    return (
+      <div className='toolbar'>
+        {filters.map((item) => (
+          <button
+            onClick={onSelectFilter}
+            key={item}
+            className={item === selected ? 'active' : ''}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+    );
+  }
+  
+  export default Toolbar;
