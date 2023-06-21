52cc8cab0650b567ccba4be7ff95b47bd39415fc
Alex Dar <dar3105@mail.ru>
2023-06-21 13:58:20 +0800

    layouts commit

---
+++ layouts/src/components/CardsView.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../css/CardsView.css'
+import ShopCard from "./ShopCard";
+
+function CardsView({ cards }) {
+    let indexCard = 0;
+    return (
+        <div className="cardsView">
+            {cards.map((card) => (
+                <ShopCard card={card} key={indexCard++} />
+            ))}
+        </div>
+    );
+}
+
+export default CardsView
